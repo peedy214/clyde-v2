3bc0503b276f91c2815919183f587013adb47bc4
Peedy <72423527+peedy214@users.noreply.github.com>
2020-10-27 16:32:10 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js')
+const client = new Discord.Client();
+console.log('Clyde V2 is online!');
+client.login('NzcwNDY4MjU0Nzg2Mzg3OTY5.X5eAeg.5mYo3lvTToiH-2Kbf_3Ow2pP4FQ')
+console.log('Clyde V2 is online!')
